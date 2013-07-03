3ad3513682730110b61b1b23ba7201ecd2e7eec1
vipulharsh <vipulharsh.iit@gmail.com>
2013-07-03 18:24:00 +0530

    modified wnat

--- wnat.h
+++ wnat.h
@@ -31,19 +31,20 @@ wnat_from(unsigned int n)
 static inline int
 wnat_eq(wnat x, wnat n)
 {
-	return x == n;
+	//printf("\n %2.10f %2.10f %2.10f %2.10f%d %d \n" ,x , n ,x - n ,n - x , n>x , n<x);
+	return ((((x - n) < 0.1) &&	 ((n - x) < 0.1))  || ((x == omega) && (n == omega)));
 }
 
 static inline int
 wnat_le(wnat x, wnat n)
 {
-	return x < n;
+	return ((x + 0.1) < n);
 }
 
 static inline int
 wnat_leq(wnat x, wnat n)
 {
-	return x <= n;
+	return (wnat_eq(x , n) || wnat_le(x ,n));
 }
 
 static inline wnat
