e44406262201a795a00aa50f00e69e4e99f002d7
Grégoire Sutre <gregoire.sutre@gmail.com>
2013-07-04 06:06:23 +0200

    Command-line options.

--- main.c
+++ main.c
@@ -2,58 +2,62 @@
 #include <stdlib.h>
 #include <err.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "petrinet.h"
 #include "tree.h"
 #include "covtree.h"
 #include "collection.h"
 
+/*
+ * usage:
+ *
+ *	Prints usage instructions and terminates the program.
+ */
+static void	 usage(void);
+
+static const char *progname;
 
 
-void createDotFile(FILE *dotFile , node *root){
-	
-	
-	if (dotFile!=NULL)
-	{
-		
-		fprintf(dotFile, "digraph G { \n");
-		
-		void* unprocessedNodes = list_manager.create();
-		list_manager.put(unprocessedNodes , root);
-		fprintf(dotFile , " \"%p\"   [label = \"" , root); 
-		marking_write(dotFile,root->marking);
-		fprintf(dotFile , "\"]; \n");
-	
-		while(!list_manager.empty(unprocessedNodes))
-		{
-	  
-		 node* temp = list_manager.get(unprocessedNodes);
-		 node* child = temp->child; 
-		 
-			while(child!=NULL){
-			    
-			    list_manager.put(unprocessedNodes,child);
-			    
-			    fprintf(dotFile , "\"%p\"  [label = \"" , child); 
-			    marking_write(dotFile,child->marking);
-			    fprintf(dotFile , "\"]; \n");
-			    fprintf(dotFile , "\"%p\" -> \"%p\" [color=\"red\" label=\"%s\"]\n" , temp , child ,child->action->name);
-			    
-			    if(child->cover!=NULL){
+void createDotFile(FILE *dotFile , node *root)
+{
+	void *unprocessedNodes;
+
+	assert(dotFile != NULL);
+
+	fprintf(dotFile, "digraph G { \n");
+
+	unprocessedNodes = list_manager.create();
+	list_manager.put(unprocessedNodes , root);
+	fprintf(dotFile , " \"%p\"   [label = \"" , root);
+	marking_write(dotFile,root->marking);
+	fprintf(dotFile , "\"]; \n");
+
+	while (!list_manager.empty(unprocessedNodes)) {
+
+		node* temp = list_manager.get(unprocessedNodes);
+		node* child = temp->child;
+
+		while (child!=NULL) {
+
+			list_manager.put(unprocessedNodes,child);
+
+			fprintf(dotFile , "\"%p\"  [label = \"" , child);
+			marking_write(dotFile,child->marking);
+			fprintf(dotFile , "\"]; \n");
+			fprintf(dotFile , "\"%p\" -> \"%p\" [color=\"red\" label=\"%s\"]\n" , temp , child ,child->action->name);
+
+			if (child->cover!=NULL) {
 				fprintf(dotFile , "\"%p\" -> \"%p\" [color=\"green\" style=\"dashed\"]\n" , child , child->cover);
-				}
-				
-				child = child->next;
-				}
-	    
-	    }
-	    
-		fprintf(dotFile ,"}");
-	    fclose(dotFile);
-	    list_manager.destroy(unprocessedNodes);
-   }
-	else printf("Unable to open file");
-	
+			}
+
+			child = child->next;
+		}
+
+	}
+
+	fprintf(dotFile ,"}");
+	list_manager.destroy(unprocessedNodes);
 }
 
 
@@ -65,16 +69,71 @@ void createDotFile(FILE *dotFile , node *root){
 
 int main(int argc, char *argv[])
 {
+	node *(*engine)(const net *, const colmgr *);
+	const colmgr *wlmgr;
+	char *dotfile, *txtfile;
 	FILE *fp;
 	net *PetriNet;
 	node *root;
+	int c;
 
-	if (argc != 2) {
-		errx(EXIT_FAILURE, "wrong number of arguments");
+	/* Program invocation name. */
+	progname = strrchr(argv[0], '/');
+	if (progname != NULL)
+		progname++;
+	else
+		progname = argv[0];
+
+	/* Default options. */
+	engine = covtree_original_km;
+	wlmgr = &list_manager;
+	dotfile = NULL;
+	txtfile = NULL;
+
+	if (argc < 2) {
+		usage();
 		/* NOTREACHED */
 	}
 
-	fp = fopen(argv[1], "r");
+	while ((c = getopt(argc, argv, "e:s:d:t:")) != -1) {
+		switch (c) {
+		case 'e':
+			if (strcmp(optarg, "km") == 0)
+				engine = covtree_original_km;
+			else if (strcmp(optarg, "km-red") == 0)
+				engine = covtree_reduced_km;
+			else if (strcmp(optarg, "mct") == 0)
+				engine = covtree_finkel_mct;
+			else
+				usage();
+			break;
+		case 's':
+			if (strcmp(optarg, "bfs") == 0)
+				wlmgr = &queue_manager;
+			else if (strcmp(optarg, "dfs") == 0)
+				wlmgr = &list_manager;
+			else
+				usage();
+			break;
+		case 'd':
+			dotfile = optarg;
+			break;
+		case 't':
+			txtfile = optarg;
+			break;
+		case '?':
+		default:
+			usage();
+			/* NOTREACHED */
+		}
+	}
+	argc -= optind;
+	argv += optind;
+
+	if (argc != 1)
+		usage();
+
+	fp = fopen(argv[0], "r");
 	if (fp == NULL) {
 		err(EXIT_FAILURE, "Unable to open `%s' for read", argv[1]);
 		/* NOTREACHED */
@@ -84,58 +143,49 @@ int main(int argc, char *argv[])
 
 	//printf("Number of transitions : %d \n" , (PetriNet)->trans_count);
 
-	fp = fopen("results/res.txt", "w");
-	if (fp == NULL) {
-		err(EXIT_FAILURE, "Unable to open `%s' for write", "res.txt");
-		/* NOTREACHED */
+	if (txtfile != NULL) {
+		fp = fopen(txtfile, "w");
+		if (fp == NULL) {
+			err(EXIT_FAILURE, "Unable to open `%s' for write", txtfile);
+			/* NOTREACHED */
+		}
+		petrinet_write(fp, PetriNet);
 	}
-	
-	
-	petrinet_write(fp, PetriNet);
-	
-	root = covtree_original_km(PetriNet, &list_manager);
 
-	
-	node_write(fp, root);
-	 FILE *dotFile1;
-	dotFile1= fopen("results/tree.dot","w");
-	createDotFile(dotFile1,root);
-
-
-	
-	
-	fprintf(fp," ---- cov -reduced ----- \n");
-	
-	node *root1 = covtree_reduced_km(PetriNet, &list_manager);
-	node_write(fp,root1);
-
-		fprintf(fp," ---- finkel mct ----- \n");
-   
-	FILE *dotFile2;
-	dotFile2= fopen("results/tree1.dot","w");
-	createDotFile(dotFile2,root1);
-
-
-	node *root2 = covtree_finkel_mct(PetriNet, &list_manager);
-	node_write(fp,root2);
-	fclose(fp);
-	FILE *dotFile3;
-	dotFile3= fopen("results/tree2.dot","w");
-	createDotFile(dotFile3,root2);
-
-
-
-
-
-
+	root = engine(PetriNet, wlmgr);
 
 	int result = covtree_complete(PetriNet , root);
-	
-	printf("The result is %d \n", result);
+
+	printf("The resulting tree is complete: %s\n", result ? "yes" : "no");
 //	printf("root - covered %d \n" , covtree_covers(((root->child)->next)->marking , root->child));
-    
 
+	if (txtfile != NULL) {
+		node_write(fp, root);
+		fclose(fp);
+	}
 
+	if (dotfile != NULL) {
+		fp = fopen(dotfile, "w");
+		if (fp == NULL) {
+			err(EXIT_FAILURE, "Unable to open `%s' for write", dotfile);
+			/* NOTREACHED */
+		}
+		createDotFile(fp, root);
+		fclose(fp);
+	}
 
 	return 0;
+}
+
+static void
+usage(void)
+{
+	errx(EXIT_FAILURE,
+	    "Usage: %s [options] <file>\n"
+	    "  -e {km, km-red, mct, mp}\n\t\tCoverability tree computation procedure.\n"
+	    "  -d <file>\tFile name of .dot output.\n"
+	    "  -t <file>\tFile name of .txt output.\n"
+	    "  -s {bfs, dfs}\tOrder of exploration.\n",
+	    progname);
+	/* NOTREACHED */
 }
